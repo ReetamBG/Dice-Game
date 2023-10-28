3037cc12aa31c0d8be80a7f723f65b806366d3a0
ReetamBG <r33t4m@gmail.com>
2023-10-28 15:53:22 +0530

    Finalised Project

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+const diceImage1 = document.querySelector(".dice1");
+const diceImage2 = document.querySelector(".dice2");
+const winner = document.querySelector("h1");
+
 function randomImage(){
     let x = Math.floor(6 * Math.random()) + 1;
     let y = Math.floor(6 * Math.random()) + 1;
     let image1Path = "./images/dice" + x + ".png";
     let image2Path = "./images/dice" + y + ".png";
 
-    const diceImage1 = document.querySelector(".dice1");
-    const diceImage2 = document.querySelector(".dice2");
     diceImage1.innerHTML = "Hello";
 
     diceImage1.setAttribute("src", image1Path);
     diceImage2.setAttribute("src", image2Path); 
+
+    if(x > y){
+        winner.innerHTML = "Player 1 Wins!";
+    }
+    else if(x < y){
+        winner.innerHTML = "Player 2 Wins!";
+    }
+    else{
+        winner.innerHTML = "Draw";
+    }
+
 }
 
 const btn = document.querySelector(".btn");
